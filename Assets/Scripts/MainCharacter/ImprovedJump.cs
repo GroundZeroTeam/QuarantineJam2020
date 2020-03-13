940a6c78049137ee7bcf98a2e55686148568ff21
LEVIATHAN-PC\Davide Donadio <davide.donadio94@gmail.com>
2020-03-13 23:25:59 +0100

    added some small interaction with the ground
    I think i'm going to merge the scripts, just a lil bit rusty ^_^

---
+++ Assets/Scripts/MainCharacter/ImprovedJump.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImprovedJump : MonoBehaviour
+{
+    [SerializeField]
+    private float fallMultiplier = 2.5f;
+    [SerializeField]
+    private float lowJumpMultiplier = 1.5f;
+    [SerializeField]
+
+
+    private Rigidbody2D rb;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+
+        if (rb.velocity.y < 0)
+        {
+            Console.WriteLine("case one");
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButtonDown("Jump"))
+        {
+            Console.WriteLine("case two");
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/MainCharacter/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    [Range(1, 10)]
+    private float jumpVelocity;
+    [SerializeField]
+    private int maxJumps = 1;
+
+    private int jumpCount = 0;
+
+    private Rigidbody2D rb;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            rb.velocity = Vector2.up * jumpVelocity;
+            jumpCount--;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D Col)
+    {
+        if (Col.gameObject.tag == "ground")
+        {
+            jumpCount = maxJumps;
+        }
+    }
+
+}
