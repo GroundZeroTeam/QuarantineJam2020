34abf51711ed788a3caa960c8c6e7a9a9c87a4cd
Sam Sheffres <samsheff@yandex.ru>
2020-03-13 22:36:17 -0700

    Initial Gameplay

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    [SerializeField]
+    [Range(1, 10)]
+    private float moveVelocity;
+
+    [SerializeField]
+    [Range(1, 10)]
+    private float destroyAfterSeconds = 5.0f;
+
+    private Transform centerPoint;
+
+    private Vector2 direction = Vector2.zero;
+
+    private Rigidbody2D rb;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        centerPoint = GameObject.Find("Center Ground").transform;
+    }
+
+    private void Start()
+    {
+        if ((centerPoint.position.x - transform.position.x) > 0.0f)
+        {
+            direction = new Vector2(moveVelocity, -5f);
+        }
+        else if ((centerPoint.position.x - transform.position.x) < 0.0f)
+        {
+            direction = new Vector2(-moveVelocity, -5f);
+        }
+
+        StartCoroutine(WaitForDestroy());
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = direction;
+
+    }
+
+    IEnumerator WaitForDestroy()
+    {
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(destroyAfterSeconds);
+
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [SerializeField]
+    [Range(0, 10)]
+    private float spawnTimeRangeMax;
+
+    [SerializeField]
+    [Range(0, 10)]
+    private float spawnTimeRangeMin;
+
+    [SerializeField]
+    private bool randomizeSpawnTime = false;
+
+    [SerializeField]
+    private GameObject spawnObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(WaitForSpawn());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator WaitForSpawn()
+    {
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(Random.Range(spawnTimeRangeMin, spawnTimeRangeMax));
+
+        Instantiate(spawnObject, transform.position, Quaternion.identity);
+
+        StartCoroutine(WaitForSpawn());
     }
 }
